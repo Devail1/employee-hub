852df9c9750674b13f00d44637fe701d4bc2af91
Liav Edry <liav.acc@gmail.com>
2024-03-07 12:09:17 +0200

    working on deployment

--- index.js
+++ index.js
@@ -3,15 +3,23 @@ const cors = require("cors");
 const { PrismaClient } = require("@prisma/client");
 
 const app = express();
-const port = process.env.PORT || 5000;
 
 const prisma = new PrismaClient();
 
 app.use(cors({ origin: process.env.DOMAIN_URL }));
 app.use(express.json());
 
+require("./routes/employees")(app, prisma);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/dist"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
-
-require("./routes/employees")(app, prisma);
