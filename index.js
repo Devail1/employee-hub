c2392fdcea425f2a19f656c83525781eb7cfaef7
Liav Edry <liav.acc@gmail.com>
2024-03-10 07:02:50 +0200

    update index

--- index.js
+++ index.js
@@ -11,13 +11,9 @@ app.use(express.json());
 
 require("./routes/employees")(app, prisma);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/dist"));
-}
+app.use(express.static("client/dist"));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
-
-module.exports = app;
